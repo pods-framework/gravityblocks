8b1121731865080774c371aca9aa34f8d3e715d1
Travis Lopes <travis.lopes@me.com>
2017-12-13 21:16:46 -0500

    Update form preview

--- js/block.js
+++ js/block.js
@@ -3,7 +3,7 @@ const { registerBlockType } = wp.blocks;
 const el = wp.element.createElement;
 const { SelectControl, ToggleControl } = wp.blocks.InspectorControls;
 const InspectorControls = wp.blocks.InspectorControls;
-const { Placeholder, SandBox } = wp.components;
+const { Placeholder, SandBox, Spinner } = wp.components;
 const Component = wp.element.Component;
 
 registerBlockType( 'gravityforms/block', {
@@ -29,7 +29,7 @@ registerBlockType( 'gravityforms/block', {
 		},
 		ajax:        {
 			type:    'bool',
-			default: true
+			default: false
 		}
 	},
 
@@ -42,54 +42,93 @@ registerBlockType( 'gravityforms/block', {
 			this.setFormId = this.setFormId.bind( this );
 			this.updateFormPreview = this.updateFormPreview.bind( this );
 
-			this.updateFormPreview();
+			this.state = {
+				html:     '',
+				fetching: false,
+			}
 
 		}
 
 		componentWillMount() {
 
-			this.setState( { html: '' } );
+			if ( this.props.attributes.formId ) {
+				this.setState( { fetching: true } );
+				this.updateFormPreview( this.props.attributes );
+			}
+
+		}
+
+		componentWillReceiveProps( props ) {
+
+			if ( props.attributes === this.props.attributes ) {
+				return;
+			}
+
+			this.updateFormPreview( props.attributes );
+
+		}
+
+		componentWillUnmount() {
+
+			this.unmounting = true;
 
 		}
 
 		setFormId( formId ) {
 
 			this.props.setAttributes( { formId: formId } );
-			this.updateFormPreview();
 
 		}
 
-		updateFormPreview() {
+		updateFormPreview( attributes ) {
 
-			let data = {
-				action:     'gform_gutenberg_preview',
-				attributes: this.props.attributes
-			};
+			if ( this.state.fetching ) {
+				return;
+			}
 
-			wp.apiRequest.transport( {
-				url:      ajaxurl,
-				method:   'GET',
-				dataType: 'json',
-				data:     data,
-				success:  function ( response ) {
-					this.setState( { html: response.data.html } )
-				}.bind( this )
-			} );
+			const { formId, title, description, ajax } = attributes;
+			const apiURL = wpApiSettings.root + 'gf/v2/block/preview?formId=' + formId + '&title=' + title + '&description=' + description + '&ajax=' + ajax;
+
+			this.setState( { fetching: true } );
+
+			window.fetch( apiURL ).then(
+				( response ) => {
+
+					if ( this.unmounting ) {
+						return;
+					}
+
+					response.json().then( ( obj ) => {
+
+						if ( obj.success ) {
+							this.setState( { html: obj.data.html } );
+						} else {
+							this.setState( { html: '<p>' + __( 'Could not load form.', 'gravityforms' ) + '</p>' } );
+						}
+
+						this.setState( { fetching: false } );
+
+					} );
+
+				}
+			);
 
 		}
 
 		render() {
 
+			const { html, fetching } = this.state;
 			const { formId, title, description, ajax } = this.props.attributes;
 			const { setAttributes, focus } = this.props;
 
 			const toggleTitle = () => setAttributes( { title: !title } );
 			const toggleDescription = () => setAttributes( { description: !description } );
 			const toggleAjax = () => setAttributes( { ajax: !ajax } );
+
 			const setFormIdFromPlaceholder = ( e ) => this.setFormId( e.target.value );
 
-			return [
-				!!focus && (
+			const controls = [
+				focus && (
 					<InspectorControls key="inspector">
 						<SelectControl
 							label={__( 'Select form', 'gravityforms' )}
@@ -113,8 +152,23 @@ registerBlockType( 'gravityforms/block', {
 							onChange={toggleAjax}
 						/>
 					</InspectorControls>
-				),
-				(!!focus || !formId) && (
+				)
+			];
+
+			if ( fetching ) {
+				return [
+					controls,
+					<div key="loading" className="wp-block-embed is-loading">
+						<Spinner/>
+						<p>{__( 'Loading form preview...', 'gravityforms' )}</p>
+					</div>
+				];
+			}
+
+			if ( !html ) {
+
+				return [
+					controls,
 					<Placeholder key="placeholder" className="wp-block-embed"
 								 label={__( 'Select a Form', 'gravityforms' )}>
 						<form>
@@ -125,11 +179,16 @@ registerBlockType( 'gravityforms/block', {
 							</select>
 						</form>
 					</Placeholder>
-				),
-				!focus && formId && (
-					<SandBox html={this.state.html}/>
-				)
-			]
+				];
+
+			}
+
+			return [
+				controls,
+				<div className="wp-block-embed__wrapper">
+					<SandBox html={html} type="html"/>
+				</div>
+			];
 
 		}
 
